e64cd7e2dec8deee3b016b7711f7363a02215e3a
Shubham Goel <shubhamgoel9512@gmail.com>
2016-12-23 16:29:19 +0000

    modified limits of search.php

--- public/search.php
+++ public/search.php
@@ -15,7 +15,7 @@
     $places = [];
     // TODO: search database for places matching $_GET["geo"]
     $places = CS50::query("SELECT * FROM places WHERE MATCH(country_code,postal_code, place_name, admin_name1,admin_code1)
-		     AGAINST (?) LIMIT 40", $_GET["geo"]);
+		     AGAINST (?) LIMIT 30", $_GET["geo"]);
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
